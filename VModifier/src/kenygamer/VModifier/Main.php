810885d6077c0d580cf86171b57c671679e3b523
Kevin Andrews <kenygamer@users.noreply.github.com>
2017-08-31 21:01:08 -0300

    Update Main.php

--- VModifier/src/kenygamer/VModifier/Main.php
+++ VModifier/src/kenygamer/VModifier/Main.php
@@ -1,1 +1,68 @@
+<?php
+/*
+ * VModifier plugin for PocketMine-MP
+ * Copyright (C) 2017 Kevin Andrews <https://github.com/kenygamer/pmmp-plugins/blob/master/V>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
 
+namespace VModifier;
+
+use pocketmine\command\{Command, CommandSender, CommandExecutor};
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase implements Listener{
+  
+  /** Plugin name */
+  const PLUGIN_NAME = "VModifier";
+  /** Plugin version */
+  const PLUGIN_VERSION = "2.0";
+  
+  const MSG_NONE = 0;
+  
+  /**
+   * @return void
+   */
+  public function onEnable(){
+    $this->getLogger()->info(TF::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
+    new AutoNotifier($this);
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  /**
+   * @return void
+   */
+  public function onDisable(){
+    $this->getLogger()->info(TF::RED."Disabling ".$this->getDescription()->getFullName()."...");
+  }
+  
+  public function onPlayerCommandPreprocess(PlayerCommandPreprocessEvent $event){
+    $player = $event->getPlayer();
+    $command = explode(" ", strtolower($event->getMessage()));
+    switch($command[0]){
+      case "/version":
+        $message = (string) $this->getConfig()->get("messages")["version"];
+        break;
+      case "/ver":
+        $message = (string) $this->getConfig()->get("messages")["ver"];
+        break;
+      default:
+        $message = self::MSG_NONE;
+    }
+    if($message !== self::MSG_NONE){
+      $player->sendMessage($message);
+    }
+  }
+  
+}
